cf5f0b234470521dae417bfd91bff6b0e9e285e4
정성훈 <127727035+Hoon94064@users.noreply.github.com>
2023-05-02 13:23:45 +0900

    Add files via upload

---
+++ 0502/0502ex2.py
@@ -0,0 +1,23 @@
+'''
+2023.05.02
+정성훈
+range()함수 - 순차적인 범위의 숫자 생성하는 함수
+'''
+
+for i in range(10): #반복 조건
+    print(i,end=' ') #반복 조건 참인 동안 반복
+
+print()
+
+for i in range(1,10):
+    print(i,end=' ')
+
+print()
+
+for i in range(1,11,2):
+    print(i,end=' ') #홀수 출력
+
+print()
+
+for i in range(10,0,-1): #반복조건
+    print(i,end=' ') #역순 출력
